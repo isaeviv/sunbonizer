ce795f090efb7f3f2a56053e39a11ff5defd6e00
isaeviv <isaeviv@vivaldi.net>
2023-04-19 22:17:47 +0700

    spec

--- spec/sunbonizer_spec.cr
+++ spec/sunbonizer_spec.cr
@@ -1,9 +1,13 @@
 require "./spec_helper"
 
 describe Sunbonizer do
-  # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  it "sunbonize" do
+    subject = Sunbonizer.sunbonize("Геннадий Чернецов ведёт свой видеоблог на YouTube, куда выкладывает не только песни, но и мельчайшие подробности личной жизни[13]. Ролики у Пророка Санбоя порой очень необычные и странные, посвящены обычно готовке, бытовой жизни, конфликтам с другими деятелями интернет-блоггинга и просто отдыху и проведению досуга типичного пенсионера.")
+    subject.should contain('=')
+    subject.should contain('-')
+    subject.should_not contain('о')
+    subject.should_not contain('е')
   end
+
 end
--- src/sunbonizer.cr
+++ src/sunbonizer.cr
@@ -1,6 +1,65 @@
 # TODO: Write documentation for `Sunbonizer`
 module Sunbonizer
+  extend self
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  def sunbonize(string : String)
+    result = ""
+    string.each_char_with_index do |c, i|
+      result += case c
+        when ' '
+          whitespace_fallback
+        when 'б'
+          b_fallback
+        when 'г'
+          g_fallback
+        when 'к'
+          k_fallback
+        when 'о'
+          o_fallback
+        when 'е'
+          e_fallback
+        when ','
+          comma_fallback
+        when '.'
+          dot_fallback
+        else
+          c
+      end
+    end
+    return result.downcase
+  end
+
+  def whitespace_fallback
+    [" ","=", " ","==", "--", " "].sample
+  end
+
+  def b_fallback
+    ['ь','б'].sample
+  end
+  
+  def g_fallback
+    ['г', 'х', 'г'].sample
+  end
+  
+  def k_fallback
+    ['к', 'х', 'к'].sample
+  end
+
+  def o_fallback
+    '0'
+  end
+
+  def e_fallback
+    'э'
+  end
+
+  def dot_fallback
+    ".."
+  end
+
+  def comma_fallback
+    ",,"
+  end
+
 end
